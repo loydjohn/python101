065e872da6e9b7b7acbbb956c10b7eed52982ea1
loydjohn <loydjohn@gmail.com>
2016-08-24 14:45:40 +0530

    Simple program to return the day of the week

---
+++ dayofweek.py
@@ -0,0 +1,18 @@
+print ("Input an Integer between 1 -7")
+day = input()
+if day==1:
+    print("1 is for Monday")
+elif day==2:
+    print("2 is for Tuesday")
+elif day==3:
+    print("3 is for Wenesday")
+elif day==4:
+    print("4 is for Thursday")
+elif day==5:
+    print("5 is for Friday")
+elif day==6:
+    print("6 is for Saturday")
+elif day==7:
+    print("7 is for Sunday")
+else:
+    print("Wrong argument passed:")
