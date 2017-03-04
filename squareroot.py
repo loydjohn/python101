7491b5a370e5b59a766182b4635fa30d6e4a3cfa
Loyd <loydjohn@gmail.com>
2017-03-04 17:47:12 +0530

    Create squareroot.py

---
+++ squareroot.py
@@ -0,0 +1,4 @@
+# Python Program to calculate the square root
+number = float(input("ENTER THE NUMBER TO COMPUTE THE SQUARE ROOT:"))
+squareroot = number ** 0.5
+print('THE SQUARE ROOT OF {0} is {1}'.format(number, squarero
